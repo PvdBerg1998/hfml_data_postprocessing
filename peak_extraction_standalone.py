5a6ba69db19a717b97448c31f46a2561d04a28e8
PvdBerg1998 <PvdBerg1998@users.noreply.github.com>
2022-09-19 14:08:22 +0200

    Prepare for publication

--- peak_extraction_standalone.py
+++ peak_extraction_standalone.py
@@ -4,14 +4,17 @@
 #   It is recommended to use a Jupyter notebook instead.
 #
 #   Copyright (C) 2021 Pim van den Berg
+#
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
+#
 #   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
+#
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -26,9 +29,6 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 from scipy.optimize import curve_fit
-
-
-# todo: extract this to a library for use in a Jupyter notebook
 
 
 #
